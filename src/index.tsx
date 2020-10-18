13b702b39d1ec2a0d06406e68b0ccd196f52e800
Tejash Patel <tejashpatel3402@gmail.com>
2020-10-18 13:49:36 -0400

    implement react-router and clean some create-react-app files

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Registration from './pages/registration';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'; 
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => (
+  <Router> 
+    <Switch>
+      <Route path="/" exact component={Registration}/> 
+    </Switch>
+  </Router>
+); 
+
+ReactDOM.render(<App/>, document.getElementById('root')); 
