2237e61658557585bf6215f515669a795c14c2f6
Omar <omar.garciaf247@gmail.com>
2020-12-05 13:49:36 -0500

    setting up systems and containers

--- src/pages/registration.tsx
+++ src/pages/registration.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
 import Button from '../components/button'
+import { colors } from '../shared/colors';
+
+const theme = 'dark';
+
+const Container = styled.div`
+  min-width: 100%;
+  min-height: 100%;
+  max-width: 1200px;
+  background-color: ${theme === 'dark' 
+    ? colors.spaceDark : colors.white };
+`;
 
 function Registration() {
   return (
-    <div>
-        <h1> Registration </h1>
-        <Button disabled></Button>
-    </div>
+    <Container>
+      <h1> Registration </h1>
+      <Button disabled></Button>
+    </Container>
   );
 }
 
---
+++ src/shared/colors.tsx
@@ -0,0 +1,11 @@
+export const colors = {
+  white: '#F5F9FF',
+  snow: '#C1DAFF',
+  ikeaBlue: '#315A96',
+  spaceGrey: '#21273F',
+  spaceDark: '#040816',
+  wine: '#89072E',
+  vanilla: '#FECD7D',
+  parksCanada: '#3A5154',
+  wineDark: '#CA4353'
+};
